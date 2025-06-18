d9281d793a71486da632f934259289aeb8046d69
piprock <yura.losov@gmail.com>
2025-06-18 23:53:27 +0300

    Добавлено IAccountService, DTOs

---
+++ Core/DTOs/UserCreateDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Core.Entities.DTOs
+{
+    public class UserCreateDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public string Role { get; set; }
+
+        public int? BankId { get; set; }
+        public int? RegionId { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
