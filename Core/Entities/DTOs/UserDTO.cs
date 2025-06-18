fce54d4c3f09da1030bceb21b8cd5f6e5fb64862
piprock <yura.losov@gmail.com>
2025-06-18 22:23:02 +0300

    Додав IAccountService та DTOs

---
+++ Core/Entities/DTOs/UserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Entities.DTOs
+{
+    public class UserDTO
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ Core/Entities/DTOs/UserEditDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Entities.DTOs
+{
+    public class UserEditDTO
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public string Role { get; set; }
+    }
+}
