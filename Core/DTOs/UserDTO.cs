f614dcea7d115170193e2391a979588789b16e90
piprock <yura.losov@gmail.com>
2025-06-18 23:59:55 +0300

    Оновив DTO: додано PhoneNumber, BankName, RegionName

--- Core/DTOs/UserDTO.cs
+++ Core/DTOs/UserDTO.cs
@@ -6,5 +6,9 @@
         public string Email { get; set; }
         public string FullName { get; set; }
         public string Role { get; set; }
+
+        public string BankName { get; set; }
+        public string RegionName { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
