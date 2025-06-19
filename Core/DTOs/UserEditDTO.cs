5d3bb21144b263b2f7cbb9bb732ae089ce732fe8
piprock <yura.losov@gmail.com>
2025-06-19 23:33:26 +0300

    додано поля які використовувались в AccountService, та зміна простору імен на namespace Core.DTOs

--- Core/DTOs/UserEditDTO.cs
+++ Core/DTOs/UserEditDTO.cs
@@ -1,14 +1,21 @@
-﻿namespace Core.Entities.DTOs
+﻿namespace Core.DTOs
 {
     public class UserEditDTO
     {
         public string Id { get; set; }
         public string Email { get; set; }
+
         public string FullName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string ImagePath { get; set; }
+
         public string Role { get; set; }
 
         public int? BankId { get; set; }
         public int? RegionId { get; set; }
         public string PhoneNumber { get; set; }
+
     }
 }
